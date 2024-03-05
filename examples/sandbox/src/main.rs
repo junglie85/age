670086ab8688a02f4ff6441583477783112b739b
junglie85 <junglie85@gmail.com>
2024-03-05 20:31:15 +0000

    Back to start again

--- examples/sandbox/src/main.rs
+++ examples/sandbox/src/main.rs
@@ -1,35 +1,6 @@
 use std::process::ExitCode;
 
-use age::{App, Color, Error, Game};
-
-struct Sandbox {}
-
-impl Game for Sandbox {
-    fn on_start(_app: &mut App) -> Result<Self, Error> {
-        Ok(Self {})
-    }
-
-    fn on_update(&mut self, app: &mut App) {
-        let window = app.window_draw_target();
-        let buf = app.begin_render_pass(window, Some(Color::RED), |renderer, rpass| {
-            renderer.draw_filled_rect(rpass);
-        });
-        app.update_display(buf);
-    }
-}
-
 fn main() -> ExitCode {
-    age::run::<Sandbox>(1920, 1080, 1, 1)
+    println!("Age");
+    ExitCode::SUCCESS
 }
-
-// draw_pixel()
-// draw_line()
-// draw_circle()
-// draw_filled_circle()
-// draw_textured_circle()
-// draw_rect()
-// draw_filled_rect()
-// draw_textured_rect()
-// draw_sprite()
-// draw_sprite_rect()
-// draw_text()

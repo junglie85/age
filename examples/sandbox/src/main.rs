8e2b31d3a47160566e2c499ffa8bb7cad99f8e1c
junglie85 <junglie85@gmail.com>
2024-03-19 09:01:09 +0000

    Working on displaying draw target in center of screen

--- examples/sandbox/src/main.rs
+++ examples/sandbox/src/main.rs
@@ -208,15 +208,30 @@ impl Game for Sandbox {
 
         ctx.pop_matrix();
 
+        let (window_width, window_height) = ctx.window_size();
+
+        let w = window_width as f32 / self.draw_target.width() as f32;
+        let h = window_height as f32 / self.draw_target.height() as f32;
+        let scale = w.min(h);
+
+        let screen_center = v2(window_width as f32, window_height as f32) / 2.0;
+        let buffer_center = v2(self.draw_target.width() as f32, self.draw_target.height() as f32) / 2.0;
+        let origin = buffer_center * scale;
+
+        let matrix = Mat4::from_translation((screen_center - origin).extend(0.0))
+            * Mat4::from_scale(Vec2::splat(scale).extend(1.0));
+
         ctx.set_draw_target(ctx.window_target());
         ctx.clear(Color::GREEN);
+        ctx.push_matrix(matrix);
         ctx.draw_textured_rect(
-            v2(0.0, 0.0),
+            Vec2::ZERO,
             0.0,
             v2(self.draw_target.width() as f32, self.draw_target.height() as f32),
             Vec2::ZERO,
             &self.draw_target_bg,
         );
+        ctx.pop_matrix();
     }
 
     fn on_stop(&mut self, _ctx: &mut Context) {}
